4c12cf59aa9a26557384d5253399825d016f95a0
EtinRuyi <omoruyi.idowu@yahoo.com>
2023-09-09 09:59:24 +0100

    Bank Model app with all functionality

---
+++ BM.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using BM.Core.Presentation;
+
+namespace BM.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomePage.RunBankController();
+        }
+    }
+}
